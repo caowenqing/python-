1a0488eb651e7c81775276e119c5f4b751c276f0
caowenqing <caowenqing111@163.com>
2018-11-14 17:39:15 +0800

    Create multiply.py
    以下函数允许计算两个数的乘积，请稍加改造，变成可接收一个或多个数并计算乘积.

---
+++ multiply.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+def product(*x):
+    result=1
+    if(len(x)!=0):
+        for item in x:
+            result=result*item
+        return result
+    else:
+        raise TypeError
+        
+ # 测试
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
