7f0be582333ace573bca7d6a18180f53a7bbda76
caowenqing <caowenqing111@163.com>
2018-11-15 08:50:44 +0800

    Update equation_x.py

--- equation_x.py
+++ equation_x.py
@@ -4,20 +4,20 @@ import math
 
 def quadratic(a, b, c):
     if(a==0):
-        x1=x2=format(-c/b,'.2f')
+        x1=x2=-c/b,
         return (x1)
     else:
         delta=b*b-4*a*c
         if(delta<0):
             print("此方程无解")
         else:
-            x1=format((-b+math.sqrt(delta))/(2*a),'.2f')
-            x2=format((-b-math.sqrt(delta))/(2*a),'.2f')
+            x1=(-b+math.sqrt(delta))/(2*a)
+            x2=(-b-math.sqrt(delta))/(2*a)
             return (x1,x2)
 
 
 #测试代码
-print('quadratic(0, 3, 1) =', quadratic(0, 3, 1))
+print('quadratic(0, 1, 1) =', quadratic(0, 1, 1))
 print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
 print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
 
