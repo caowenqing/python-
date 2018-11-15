cbf2fb2d9125fa58e8142e810b8f9f2247cacfc4
caowenqing <caowenqing111@163.com>
2018-11-15 08:55:37 +0800

    Update equation_x.py

--- equation_x.py
+++ equation_x.py
@@ -21,7 +21,7 @@ print('quadratic(0, 1, 1) =', quadratic(0, 1, 1))
 print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
 print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
 
-if quadratic(0, 1, 1) != -1.0:
+if quadratic(0, 1, 1) != -1:
     print('测试失败')
 elif quadratic(2, 3, 1) != (-0.5, -1.0):
     print('测试失败')
