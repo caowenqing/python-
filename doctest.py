5e2b99b64290a7c3366093efa0fd05769b666e1d
caowenqing <caowenqing111@163.com>
2018-12-06 10:01:20 +0800

    Create doctest.py
    对函数fact(n)编写doctest并执行

---
+++ doctest.py
@@ -0,0 +1,26 @@
+# 对函数fact(n)编写doctest并执行：
+
+# -*- coding: utf-8 -*-
+
+def fact(n):
+    '''
+    Calculate 1*2*...*n
+
+    >>> fact(1)
+    1
+    >>> fact(10)
+    3628800
+    >>> fact(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError
+    '''
+    if n < 1:
+        raise ValueError()
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
