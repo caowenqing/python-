14642906c95212e3aa97489771e9d0093fccee77
caowenqing <caowenqing111@163.com>
2018-12-10 15:54:50 +0800

    Create urllib.py
    对豆瓣的一个URLhttps://api.douban.com/v2/book/2129650进行抓取，并返回响应

---
+++ urllib.py
@@ -0,0 +1,11 @@
+import ssl
+from urllib import request
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
